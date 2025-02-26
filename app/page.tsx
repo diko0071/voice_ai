d2be69fe217f5cca109158b07f03e26bd70bb2cd
kirill-markin <markinkirill@gmail.com>
2025-02-26 16:59:26 +0200

    remove google presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import ChatGPT from "@/app/chatgpt";
-import PresentationTest from '@/components/PresentationTest';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PresentationTest />
+      <ChatGPT />
     </main>
   );
 }
